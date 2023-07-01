0fbbcd1d69bd6192e896ad23f8f0bf01639545bd
PramodKumarGH <pramodkumargh77@gmail.com>
2023-07-01 21:24:31 +0530

    Added main project

---
+++ tailwindcss-and-nextjs-portfolio-final/components/ProjectsSection.tsx
@@ -0,0 +1,72 @@
+import React from "react"
+import Image from "next/image"
+import Link from "next/link"
+import SlideUp from "./SlideUp"
+import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs"
+
+const projects = [
+  {
+        name: "Fire detection using Thermal Camera",
+    description:
+            "Developed a system which detects and tracks fire using deep learning and neural networks.",
+    image: "/fire.jpg",
+    github: "https://github.com/hqasmei/thankful-thoughts",
+    link: "https://thankfulthoughts.io/",
+  },
+  {
+      name: "Automatic Movie Recommender System",
+      description: "This project leverages a dataset of 5,000 movies with 16 variables from the movie database to predict movie revenues.",
+    image: "/movie.jpg",
+    github: "https://github.com/hqasmei/platoio",
+    link: "https://platoio.com/register",
+  },
+  
+]
+
+const ProjectsSection = () => {
+  return (
+    <section id="projects">
+      <h1 className="my-10 text-center font-bold text-4xl">
+        Projects
+        <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+      </h1>
+
+      <div className="flex flex-col space-y-28">
+        {projects.map((project, idx) => {
+          return (
+            <div key={idx}>
+              <SlideUp offset="-300px 0px -300px 0px">
+                <div className="flex flex-col  animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
+                  <div className=" md:w-1/2">
+                    <div >
+                      <Image
+                        src={project.image}
+                        alt=""
+                        width={1000}
+                        height={1000}
+                        className="rounded-xl shadow-xl hover:opacity-70"
+                      />
+                    </div>
+                  </div>
+                  <div className="mt-8 md:w-1/2">
+                    <h1 className="text-4xl font-bold mb-6">{project.name}</h1>
+                    <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
+                      {project.description}
+                    </p>
+                    <div className="flex flex-row align-bottom space-x-4">
+                      
+                      
+                    </div>
+                  </div>
+                </div>
+              </SlideUp>
+            </div>
+          )
+        })}
+        
+      </div>
+    </section>
+  )
+}
+
+export default ProjectsSection
